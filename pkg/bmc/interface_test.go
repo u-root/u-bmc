1a38ecc524c231773bf3e4f073b8d53853dff750
Christian Svensson <bluecmd@google.com>
2018-11-03 15:21:04 +0100

    Reset interface on MAC change
    If the MAC address change the IPv6 link-local address is not recomputed
    without flapping the interface. Since NC-SI boots with a random MAC and
    corrects this later in boot this happens every boot.
    
    Signed-off-by: Christian Svensson <bluecmd@google.com>

---
+++ pkg/bmc/interface_test.go
@@ -0,0 +1,27 @@
+// Copyright 2018 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package bmc
+
+import (
+	"net"
+	"testing"
+)
+
+func TestIPv6MACCheck(t *testing.T) {
+	ip_ok := net.ParseIP("fe80::5054:ff:fe12:3456")
+	ip_wrong := net.ParseIP("fec0::5054:ff:fe12:3456")
+	mac_ok, _ := net.ParseMAC("52:54:00:12:34:56")
+	mac_wrong, _ := net.ParseMAC("52:54:00:12:34:57")
+
+	if !isLinkLocalForMAC(ip_ok.To16(), mac_ok) {
+		t.Errorf("Expected %v to be the MAC for link-local IP %v", mac_ok, ip_ok)
+	}
+	if isLinkLocalForMAC(ip_wrong.To16(), mac_ok) {
+		t.Errorf("Expected IP %v to not be link-local", ip_wrong)
+	}
+	if isLinkLocalForMAC(ip_ok.To16(), mac_wrong) {
+		t.Errorf("Expected %v to not be the MAC for link-local IP %v", mac_wrong, ip_ok)
+	}
+}
