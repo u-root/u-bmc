598f2e8738244936d8d031d4a929afdeedf957bd
Christian Svensson <bluecmd@google.com>
2018-11-05 16:05:59 +0100

    Set readthedocs theme for local builds

--- doc/conf.py
+++ doc/conf.py
@@ -74,7 +74,11 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# Use the following theme for a minimal setup
+# html_theme = 'alabaster'
+
+# Use the same theme as Read the Docs uses
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
