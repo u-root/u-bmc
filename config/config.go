3b3e7bd601bf89e737734f564f04b40f42a74ef8
Marvin Drees <marvin.drees@9elements.com>
2021-11-02 15:55:17 +0100

    Change roughtime server addresses
    Proper IPv4 and v6 detection werks so we don't need
    duplicate entries anymore. Also add some more server
    addresses to the list
    
    Signed-off-by: Marvin Drees <marvin.drees@9elements.com>

--- config/config.go
+++ config/config.go
@@ -29,32 +29,26 @@ type Config struct {
 	ACME                ACME
 }
 
-var (
-	DefaultConfig = &Config{
-		// The philosophy behind the time configuration is to use a fast, simple, and
-		// authenticated time protocol for the initial time configuration followed
-		// by a refined adjustment by (unauthenticated) NTP for precision.
-		// This severely limits how much an NTP server can lie to u-bmc (+/- a few
-		// seconds) which makes it a mild inconvenience when reading logs but that's
-		// about it.
-		//
-		// Other BMCs choose to trust the host's clock but given that the host
-		// could be compromised and trying to fool the BMC into accepting a
-		// bad time source that's not what u-bmc does.
-		//
-		// Instead, u-bmc will refuse to execute any remote actions until an accurate
-		// time source has been established.
-		RoughtimeServers: []ttime.RoughtimeServer{
-			// TODO(bluecmd): Doubled entries due to https://github.com/cloudflare/roughtime/issues/10
-			{Protocol: "udp4", Address: "roughtime.cloudflare.com:2002", PublicKeyType: ttime.KEY_TYPE_ED25519, PublicKey: "gD63hSj3ScS+wuOeGrubXlq35N1c5Lby/S+T7MNTjxo="},
-			{Protocol: "udp6", Address: "roughtime.cloudflare.com:2002", PublicKeyType: ttime.KEY_TYPE_ED25519, PublicKey: "gD63hSj3ScS+wuOeGrubXlq35N1c5Lby/S+T7MNTjxo="},
-			{Protocol: "udp4", Address: "roughtime.sandbox.google.com:2002", PublicKeyType: ttime.KEY_TYPE_ED25519, PublicKey: "etPaaIxcBMY1oUeGpwvPMCJMwlRVNxv51KK/tktoJTQ="},
-			{Protocol: "udp6", Address: "roughtime.sandbox.google.com:2002", PublicKeyType: ttime.KEY_TYPE_ED25519, PublicKey: "etPaaIxcBMY1oUeGpwvPMCJMwlRVNxv51KK/tktoJTQ="},
-		},
-		// While u-bmc has been granted 0.u-bmc.pool.ntp.org, they do not currently
-		// offer AAAA/IPv6 records. In the mean time use Google's leap smeared
-		// NTP servers that do have IPv6.
-		NtpServers: []ttime.NtpServer{"time1.google.com", "time2.google.com", "time3.google.com", "time4.google.com"},
+var DefaultConfig = &Config{
+	// The philosophy behind the time configuration is to use a fast, simple, and
+	// authenticated time protocol for the initial time configuration followed
+	// by a refined adjustment by (unauthenticated) NTP for precision.
+	// This severely limits how much an NTP server can lie to u-bmc (+/- a few
+	// seconds) which makes it a mild inconvenience when reading logs but that's
+	// about it.
+	//
+	// Other BMCs choose to trust the host's clock but given that the host
+	// could be compromised and trying to fool the BMC into accepting a
+	// bad time source that's not what u-bmc does.
+	//
+	// Instead, u-bmc will refuse to execute any remote actions until an accurate
+	// time source has been established.
+	RoughtimeServers: []ttime.RoughtimeServer{
+		{Protocol: "udp", Address: "roughtime.cloudflare.com:2002", PublicKeyType: ttime.KEY_TYPE_ED25519, PublicKey: "gD63hSj3ScS+wuOeGrubXlq35N1c5Lby/S+T7MNTjxo="},
+		{Protocol: "udp", Address: "roughtime.sandbox.google.com:2002", PublicKeyType: ttime.KEY_TYPE_ED25519, PublicKey: "etPaaIxcBMY1oUeGpwvPMCJMwlRVNxv51KK/tktoJTQ="},
+		{Protocol: "udp", Address: "time.0xt.ca:2002", PublicKeyType: ttime.KEY_TYPE_ED25519, PublicKey: "iBVjxg/1j7y1+kQUTBYdTabxCppesU/07D4PMDJk2WA="},
+		{Protocol: "udp", Address: "roughtime.int80h.com:2002", PublicKeyType: ttime.KEY_TYPE_ED25519, PublicKey: "AW5uAoTSTDfG5NfY1bTh08GUnOqlRb+HVhbJ3ODJvsE="},
+	},
 
 		// Enable this to have the SSH server start on bootup.
 		// This is useful if you're debugging startup problems in u-bmc.
